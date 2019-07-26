470cb59e98d3e8670e8ac93f57425ec03cc483f9
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2019-07-26 19:38:39 +0530

    adding Link tags to navigate through components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 
 const PageOne = () => {
     return (
-        <div>Page One</div>
+        <div>
+            Page One<br />
+            <Link to="/pagetwo">Navigate to page two</Link>
+        </div>
     )
 }
 const PageTwo = () => {
     return (
-        <div>Page Two</div>
+        <div>
+            <div>
+                Page Two<br />
+                <Link to="/">Navigate to page one</Link>
+            </div>
+        </div>
     )
 }
 
